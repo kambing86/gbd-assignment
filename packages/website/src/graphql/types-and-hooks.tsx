83a26d7be269ecb67ba5e98d550687f6a7c54b96
Chua Kang Ming <kambing860210@gmail.com>
2020-08-13 14:00:29 +0800

    chore: fix graphql generate issue in server

--- packages/website/src/graphql/types-and-hooks.tsx
+++ packages/website/src/graphql/types-and-hooks.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable */
+import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-const gql = Apollo.gql;
-
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
